6a7fc883f089442b93f6cb9ea2c9d8e30034a780
bemtorres <benja.mora.torres@gmail.com>
2022-09-19 15:32:03 -0300

    init project

---
+++ resources/js/Welcome.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Hola</h1>
+    <p>{{title}}</p>
+</template>
+
+<script>
+export default {
+   setup: () => ({
+        title: 'How To Install Vue 3 in Laravel 9 From Scratch'
+    })
+}
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+
+import {createApp} from "vue";
+import Welcome from "./Welcome.vue";
+
+
+createApp(Welcome).mount("#app");
